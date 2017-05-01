c55872a6c3d74d81ce34ce8f67ce2284d1b306a2
Skyz <voxqhuy@gmail.com>
2017-05-01 16:28:49 -0400

    Star (Turtle)
    A python program uses turtle to draw a star

---
+++ star.py
@@ -0,0 +1,20 @@
+import turtle  # Make all the Turtle graphics functions availabl
+
+
+turtle.penup() # Lift pen to move it
+turtle.setposition(-200, 72) # Move the pen to coordinates (-45, 100)
+turtle.pendown()
+turtle.forward(400)
+turtle.right(144)
+turtle.forward(400)
+turtle.right(144)
+turtle.forward(400)
+turtle.right(144)
+turtle.forward(400)
+turtle.right(144)
+turtle.forward(400)
+turtle.right(144)
+turtle.hideturtle()
+turtle.exitonclick()
+turtle.update()              # Refresh window
+turtle.done()                # Wait for the user to close the window
